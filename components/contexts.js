d3ae340763edc78f44c8ecc9767809d232eac431
arek <49416568+amich1@users.noreply.github.com>
2020-05-02 14:00:41 +0100

    new features, connects to dedi server

--- components/contexts.js
+++ components/contexts.js
@@ -1,3 +1,4 @@
 import * as React from 'react';
 export const AuthContext = React.createContext();
 export const UserContext = React.createContext();
+export const CartContext = React.createContext();
